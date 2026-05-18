cd02b52e07e265d8fb40f68cf25209950f01b13d
Yaroslav <plehanov.kzn2@gmail.com>
2026-05-18 13:03:55 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+import spacy
+
+app = FastAPI()
+
+nlp = spacy.load("en_core_web_sm")
+
+
+class AnalyzeRequest(BaseModel):
+    text: str
+
+
+@app.post("/analyze")
+def analyze(request: AnalyzeRequest):
+
+    doc = nlp(request.text)
+
+    tokens = []
+
+    for token in doc:
+        tokens.append({
+            "text": token.text,
+            "lemma": token.lemma_,
+            "pos": token.pos_,
+            "dependency": token.dep_,
+            "head": token.head.text
+        })
+
+    return {
+        "tokens": tokens
+    }
